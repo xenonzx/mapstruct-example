b079daac70b9af4a6d48767b3e0aeef4360904c2
eldakhakhny <ahmed.eldakhakhny@blink22.com>
2022-04-20 15:19:58 +0200

    initial project

---
+++ README.md
@@ -0,0 +1,8 @@
+task one map product domain to dto
+
+1. add gradle dependencies
+2. create domain model 
+3. create dto model
+4. create mapper
+
+task two map dto to product
---
+++ src/test/java/com/dakhakhny/mapstruct/demo/MapstructDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dakhakhny.mapstruct.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapstructDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
